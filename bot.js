733b6768b6bc250e063c7a82722dbef818911bb1
melonmelancia <netopc53@gmail.com>
2025-02-24 07:59:42 -0300

    Instalando dependências do bot

--- bot.js
+++ bot.js
@@ -1,34 +1,21 @@
-const { makeWASocket } = require('@adiwajshing/baileys');
+const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
 const fs = require('fs');
 
-const authFile = './auth_info.json';
-
-async function start() {
-    const state = fs.existsSync(authFile) ? require(authFile) : undefined;
+(async () => {
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info');
     const sock = makeWASocket({
         auth: state,
         printQRInTerminal: true,
     });
 
-    sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect } = update;
-        if (connection === 'close') {
-            const shouldReconnect = lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut;
-            console.log('Conexão fechada. Tentando reconectar...', shouldReconnect);
-            if (shouldReconnect) {
-                start();
-            }
-        } else if (connection === 'open') {
-            console.log('Conectado ao WhatsApp!');
-        }
+    sock.ev.on('creds.update', saveCreds);
+    sock.ev.on('messages.upsert', async (m) => {
+        const msg = m.messages[0];
+        if (!msg.message) return;
+
+        const sender = msg.key.remoteJid;
+        console.log('Mensagem recebida de:', sender);
+
+        await sock.sendMessage(sender, { text: 'Olá!' });
     });
-
-    sock.ev.on('auth-state.update', (authState) => {
-        fs.writeFileSync(authFile, JSON.stringify(authState, null, 2));
-    });
-
-    await sock.connect();
-    console.log('Bot iniciado!');
-}
-
-start();
+})();
