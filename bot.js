904c15648b77b91a0a2d52d8320b6f1fc41348d3
melonmelancia <netopc53@gmail.com>
2025-02-24 08:02:34 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,24 @@
 const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
 const fs = require('fs');
+const { generate } = require('qrcode-terminal');
 
 (async () => {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info');
     const sock = makeWASocket({
         auth: state,
-        printQRInTerminal: true,
+        printQRInTerminal: false,
     });
 
     sock.ev.on('creds.update', saveCreds);
+    sock.ev.on('connection.update', (update) => {
+        const { qr } = update;
+        if (qr) {
+            console.log('Escaneie o QR Code abaixo ou abra o arquivo qr_code.png:');
+            generate(qr, { small: true });
+            fs.writeFileSync('qr_code.png', qr);
+        }
+    });
+
     sock.ev.on('messages.upsert', async (m) => {
         const msg = m.messages[0];
         if (!msg.message) return;
